93a1accc6707961b391ace968c6022006fa360c9
Ravi_Kumar <ravikumardhn26@gmail.com>
2023-01-03 10:50:31 +0530

    commit

---
+++ Java_Logic_Buildind/Basics_02/Conditionals.java
@@ -0,0 +1,12 @@
+public class Conditionals{
+    public static void main (String[] args){
+        int age = 12;
+        boolean cond = (age==18);
+        if(cond){
+            System.out.println("Yes boy you can drive!!!");
+        }
+        else{
+            System.out.println("No boy you cannot drive!!!");
+    }
+}
+}
